aea6d3e1a2efa3b5bbf74115fa329e3f762f30ab
Joel Tchoufa <joelnkouatchet@gmail.com>
2022-01-12 16:19:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
-
+//import * as Stomp from '@stomp/stompjs';
+//import * as SockJS from 'sockjs-client';
+import * as SockJS from 'sockjs-client';
+import {Stomp} from '@stomp/stompjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'tehamfront';
+
+  
+  description = 'Angular-WebSocket Demo';
+
+  greetings: string[] = [];
+  disabled = true;
+  name: string='';
+  private stompClient:any = null;
+
+  constructor() { }
+
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+
+    if (connected) {
+      this.greetings = [];
+    }
+  }
+
+  connect() {
+    const socket = new SockJS('http://localhost:8080/gkz-stomp-endpoint');
+    this.stompClient = Stomp.over(socket);
+
+    const _this = this;
+    this.stompClient.connect({}, function (frame:any) {
+      _this.setConnected(true);
+      console.log('Connected: ' + frame);
+
+      _this.stompClient.subscribe('/topic/hi', function (hello:any) {
+        _this.showGreeting(JSON.parse(hello.body).greeting);
+      });
+    });
+  }
+
+  disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+
+    this.setConnected(false);
+    console.log('Disconnected!');
+  }
+
+  sendName() {
+    this.stompClient.send(
+      '/gkz/hello',
+      {},
+      JSON.stringify({ 'name': this.name })
+    );
+  }
+
+  showGreeting(message:any) {
+    this.greetings.push(message);
+  }
 }
